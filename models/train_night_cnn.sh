870f94300158e08419ffbf142519059a7737fc76
machnev <machnev@yandex-team.ru>
2019-05-25 05:10:21 +0300

    Убрал комментарий

--- models/train_night_cnn.sh
+++ models/train_night_cnn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#models/train.sh crf_cnn_characters_only exists_ru models/crf/characters_only/cnn/lstm.jsonnet
+models/train.sh crf_cnn_characters_only exists_ru models/crf/characters_only/cnn/lstm.jsonnet
 models/train.sh crf_cnn_characters_only_bidir exists_ru models/crf/characters_only/cnn/lstm_bidir.jsonnet
 models/train.sh crf_cnn_characters_only_multi_head_self_attention exists_ru models/crf/characters_only/cnn/multi_head_self_attention.jsonnet
 models/train.sh crf_cnn_characters_only_bidirectional_language_model_transformer exists_ru models/crf/characters_only/cnn/bidirectional_language_model_transformer.jsonnet
--- utils/split_train_test.py
+++ utils/split_train_test.py
@@ -5,12 +5,12 @@ prefix = 'temp/datasets/exists_ru/exists_ru'
 
 df = pd.read_excel(prefix + '_normalized.xlsx')
 
-msk = np.random.rand(len(df)) < 0.9
+msk = np.random.rand(len(df)) < 0.67
 
 train = df[msk]
 other = df[~msk]
 
-msk_test = np.random.rand(len(other)) < 1
+msk_test = np.random.rand(len(other)) < 0.5
 test = other[msk_test]
 
 train.to_excel(prefix + '_train.xlsx')
