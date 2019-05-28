962e5bb758023e41bb0c7f2afd9e9f816356664d
machnev <machnev@yandex-team.ru>
2019-05-28 17:54:44 +0300

    Add test share to split_train_test

--- utils/split_train_test.py
+++ utils/split_train_test.py
@@ -5,23 +5,24 @@ import os
 
 dataset_file = sys.argv[1]
 
-#output_prefix = 'temp/datasets/exists_ru/exists_ru'
 output_dir = sys.argv[2]
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
 train_share = float(sys.argv[3])
+test_in_other_share = float(sys.argv[4])
 
 df = pd.read_excel(dataset_file)
 df = df.sample(frac=1).reset_index(drop=True)
 
-msk = np.random.rand(len(df)) < 0.67
+msk = np.random.rand(len(df)) < train_share
 
 train = df[msk]
 other = df[~msk]
 
-msk_test = np.random.rand(len(other)) < 0.5
+msk_test = np.random.rand(len(other)) < test_in_other_share
 test = other[msk_test]
+other = other[~msk_test]
 
 train.to_excel(output_dir + '/train.xlsx')
 test.to_excel(output_dir + '/test.xlsx')
